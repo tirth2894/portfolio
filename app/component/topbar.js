e86ab64d37ab5da8f2ab8d513cdc9fcde678fa64
tirth2894 <tirthrollwala@gmail.com>
2025-04-25 20:13:58 +0530

    Added : Search Functionalities

--- app/component/topbar.js
+++ app/component/topbar.js
@@ -19,12 +19,22 @@ import { FaRegSquare, FaMinus } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 import { PiFinnTheHumanBold } from "react-icons/pi";
 import ThemeToggle from "./theamToggle";
+import portfolioData from "../data/content";
+import { BsFileEarmarkText } from "react-icons/bs";
+import { AiOutlinePicture } from "react-icons/ai";
+import NotepadWindow from "./notePad";
+import Photos from "./photos";
 
-export default function Topbar({ path, setPath, isFullScreen, setIsFullScreen, setShowWelcome }) {  
+export default function Topbar({ path, setPath, isFullScreen, setIsFullScreen, setShowWelcome }) {
   const currentPath = path.join(" > ");
 
   const [history, setHistory] = useState([path]);
   const [currentIndex, setCurrentIndex] = useState(0);
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [openedFile, setOpenedFile] = useState(null);
+
 
   // Update history if path changes externally
   useEffect(() => {
@@ -58,10 +68,43 @@ export default function Topbar({ path, setPath, isFullScreen, setIsFullScreen, s
     }
   };
 
+  const handleSearch = (query) => {
+    setSearchQuery(query);
+    if (!query.trim()) {
+      setSearchResults([]);
+      return;
+    }
+
+    const lowerQuery = query.toLowerCase();
+
+    const results = Object.entries(portfolioData)
+      .filter(([key, value]) => {
+        const keyMatch = key.toLowerCase().includes(lowerQuery);
+        const sectionMatch = value.section?.toLowerCase().includes(lowerQuery);
+        const contentMatch =
+          value.type === "text" &&
+          value.data?.some(
+            item =>
+              item.title?.toLowerCase().includes(lowerQuery) ||
+              item.description?.toLowerCase().includes(lowerQuery)
+          );
+        return keyMatch || sectionMatch || contentMatch;
+      })
+      .map(([key, value]) => ({
+        key,
+        type: value.type,
+      }));
+
+    setSearchResults(results);
+
+    console.log(searchQuery, searchResults)
+  };
+
+
   return (
     <div className="text-[var(--text)] rounded-lg rounded-b-none pb-1 h-full">
       <div className="w-full rounded-t-xl flex justify-between items-center px-2 pt-1 border-b-0">
-        <div onClick={()=>window.location.reload()} className="flex items-center justify-center space-x-1 cursor-pointer">
+        <div onClick={() => window.location.reload()} className="flex items-center justify-center space-x-1 cursor-pointer">
           <PiFinnTheHumanBold className="ml-1" />
           <span className="text-md p-1 font-bold">Tirth's Portfolio</span>
         </div>
@@ -101,7 +144,7 @@ export default function Topbar({ path, setPath, isFullScreen, setIsFullScreen, s
             <button className="p-2 cursor-pointer">
               <BsFilter size={16} className="opacity-50" />
             </button>
-              <ThemeToggle/>
+            <ThemeToggle />
           </div>
 
         </div>
@@ -129,12 +172,60 @@ export default function Topbar({ path, setPath, isFullScreen, setIsFullScreen, s
             <input
               type="text"
               placeholder="Search Home"
+              value={searchQuery}
+              onChange={(e) => handleSearch(e.target.value)}
               className="w-full text-sm px-3 py-1 rounded outline-none border border-gray-500"
             />
             <AiOutlineSearch className="absolute right-2 top-2 text-gray-500" size={16} />
+
+            {searchResults.length > 0 && (
+              <div className="absolute left-0 w-full bg-[var(--bg)] shadow-md border border-[var(--hover)] z-50">
+                {searchResults.map(({ key, type }) => (
+                  <div
+                    key={key}
+                    onClick={() => {
+                      if (type === "text") {
+                        setOpenedFile({ key, type });
+                      } else if (type === "image") {
+                        setOpenedFile({ key, type });
+                      }
+                      setSearchQuery("");
+                      setSearchResults([]);
+                    }}
+                    className="px-3 py-2 hover:bg-[var(--hover)] border-b-2 border-[var(--hover)] cursor-pointer text-sm flex justify-between"
+                  >
+                    <span className="flex items-center max-w-full space-x-2 overflow-hidden">
+                      {type === "text" ? (
+                        <BsFileEarmarkText size={16} className="text-blue-600 shrink-0" />
+                      ) : (
+                        <AiOutlinePicture size={16} className="text-emerald-600 shrink-0" />
+                      )}
+                      <span className="truncate block">{key}</span>
+                    </span>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
+
+      {
+        openedFile && openedFile.type === "text" && (
+          <NotepadWindow
+            openedFile={openedFile.key}
+            setOpenedFile={setOpenedFile}
+          />
+        )
+      }
+      {
+        openedFile && openedFile.type === "image" && (
+          <Photos
+            openedFile={openedFile.key}
+            setOpenedFile={setOpenedFile}
+          />
+        )
+      }
     </div>
   );
 }
